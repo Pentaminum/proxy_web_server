0a3014c104c51e2ff339437f25b10d524d6d73c4
scott0929 <scott0929@gmail.com>
2023-12-01 21:34:28 -0800

    web

--- web.py
+++ web.py
@@ -2,7 +2,23 @@ import socket
 import os
     
 def handle_request(request):
-    return 0
+    lines = request.split('\r\n')
+    method, path, _ = lines[0].split(' ')
+    print(f"Method: {method}, Path: {path}")
+
+    # 404: Check if the requested resource is valid
+    if not os.path.exists(path[1:]):
+        print("No such file")
+        response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+        return response.encode()
+
+    with open('test.html', 'rb') as file:
+        content = file.read()
+        response = 'HTTP/1.1 200 OK\r\n'
+        response += f'Content-Length: {len(content)}\r\n\r\n'
+        response = response.encode() + content
+
+    return response
 
 def start_server(port):
     # Create a socket
@@ -15,23 +31,33 @@ def start_server(port):
     server_socket.listen(1)
     print(f'Server listening on port {port}...')
     
-    while True:
-        # Accept a connection
-        client_socket, client_address = server_socket.accept()
-        print(f'Accepted connection from {client_address}')
-        
-        # Receive data from the client
-        request = client_socket.recv(1024).decode()
-        
-        # Handle the request
-        response = handle_request(request)
-        
-        # Send the response back to the client
-        client_socket.sendall(response)
-        
-        # Close the connection
-        client_socket.close()
+    try:
+        while True:
+            # Accept a connection
+            client_socket, client_address = server_socket.accept()
+            print(f'\nAccepted connection from {client_address}')
+            
+            try:
+                # Receive data from the client
+                request = client_socket.recv(1024).decode()
+                
+                # Handle the request
+                response = handle_request(request)
+                
+                # Send the response back to the client
+                client_socket.sendall(response)
+                
+            except Exception as e:
+                print(f"Error handling request: {e}")
+
+            finally:
+                # Close the connection
+                client_socket.close()
+
+    except KeyboardInterrupt:
+        print("\nServer shutting down...")
+        server_socket.close()
 
 if __name__ == '__main__':
     # Start the server on port 8080
-    start_server(8080)
+    start_server(8085)
